db7d1bd269db8e780f18e0ea80dc7b82c4226767
ihell <ihell269@gmail.com>
2025-05-27 19:56:49 +0700

    add .gitignore, update firestore client initialization, and add service account key

--- firebase/firestore.go
+++ firebase/firestore.go
@@ -3,19 +3,35 @@ package firebase
 import (
 	"cloud.google.com/go/firestore"
 	"context"
+	"encoding/json"
 	"log"
+	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 )
 
 var Client *firestore.Client
 
 func InitFirestore() {
-	ctx := context.Background()
-	sa := option.WithCredentialsFile("serviceAccountKey.json")
-	client, err := firestore.NewClient(ctx, "kalo erorro lupa ganti-id", sa)
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Failed to create Firestore client: %v", err)
+		log.Fatalf("Gagal memuat .env: %v", err)
 	}
+
+	credJSON := os.Getenv("FIREBASE_CREDENTIAL")
+	projectID := os.Getenv("FIREBASE_PROJECT_ID")
+
+	// Convert string to io.Reader
+	r := strings.NewReader(credJSON)
+
+	ctx := context.Background()
+	opt := option.WithCredentialsJSON([]byte(credJSON))
+	client, err := firestore.NewClient(ctx, projectID, opt)
+	if err != nil {
+		log.Fatalf("Gagal membuat Firestore client: %v", err)
+	}
+
 	Client = client
 }
